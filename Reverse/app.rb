e3e8f2f26ca59b04c62463c1868857bcb8e6884c
Jack Hanford <jackhanford@gmail.com>
2013-04-19 11:00:04 -0700

    Got my sinatra app running, Fuck DS_Stores.

--- Reverse/app.rb
+++ Reverse/app.rb
@@ -2,21 +2,21 @@ require "rubygems"
 require "sinatra"
 
 get '/' do
+  form = "      <form action='/reverse.css'>
+        <input type='text' name='text'>
+        <input type='submit'>"
  " <html>
      <style><!-- Include CSS code here -->
      </style>
     <body>
-      <form action='/reverse'>
-        <input type='text' name='text'>
-        <input type='submit'>
+#{ form + form}
       </form>
     </body>
    </html>
    "
 
-
 end
-   get '/reverse' do
+get '/reverse.css' do
   params[:text].reverse
   
 end
