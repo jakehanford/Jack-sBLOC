27b835233876a772e2217c99cc6fd048b23ae0cf
Jack Hanford <jackhanford@gmail.com>
2013-04-22 16:36:23 -0700

    Finally got CSS!
    Added the public folder realizing the difference between Web/File
    servers.. Finished project =)

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "sinatra"
 
 get '/' do
-  form = "<form action='/reverse.css'>
+  form = "<form action='/reverse'>
           <input type='text' name='text'>
           <input type='submit'>
           </form>"
@@ -26,8 +26,44 @@ get '/' do
     <li><a href='#'>Filler</a></li>
     <li><a href='#'>Filler</a></li>
   </ul>
-  </div>"
- " <html>
+  </div>
+  </div>
+  "
+  body = "
+    <div class='span10'>
+    <div align='center'>
+    <header>
+    <div id='banner'>
+      </div>
+    </header>
+    <section>
+      <header>
+        <h3>Blog Posts</h3>
+      <hr>
+      </header>
+      <article class='well'>
+        <header>
+          <h4>Today</h4>
+        </header>
+        <div class='post'>
+         
+        </div>
+        
+        <p>I learned so much Ruby today, it was insane. Ruby is so great, and I'm having so much fun in Bloc. I really enjoy working with my mentor and cohort. I can't wait to deploy my first app!</p>
+      </article>
+      <div class='submit'>
+          <button class='btn-success' type='button'>Submit</button>
+          <button class='btn-danger' type='button'>Cancel</button>
+      </div>
+    </section>
+  </div>
+     </div>
+  </div>
+</div>
+
+    "
+ " 
+ <html>
  <head>
  <link href='bootstrap.css' rel='stylesheet' media='screen'>
      <style>
@@ -36,7 +72,10 @@ get '/' do
     <body>
     #{ sidebar }
     
+    #{ body }
     #{ form }
+
+    
     
     </body>
    </html>
